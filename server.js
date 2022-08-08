f82c2b38c92d5fee72c294752f13d6d4d4283907
aungkant <aungkhantzaw@cloudsource>
2022-08-08 15:58:42 +0630

    port change

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.use('/graphql', graphqlHTTP({
   rootValue: root,
   graphiql: true,
 }));
-app.listen(4000);
+app.listen(80);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
 
